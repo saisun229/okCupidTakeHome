37acf8782c62281eb86fc41c0dddba3d5f125df8
Michael P. Geraci <me@mgeraci.com>
2021-07-23 15:39:36 -0500

    add trailing commas

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import App from "./components/App";
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
